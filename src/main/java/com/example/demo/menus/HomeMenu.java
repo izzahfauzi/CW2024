116a15137d43e3cfc0b416e9e0f6f714873f3bb4
izzahfauzi <nurul.izzahx25@gmail.com>
2024-12-03 05:02:50 +0800

    Added exit button in home menu

--- src/main/java/com/example/demo/menus/HomeMenu.java
+++ src/main/java/com/example/demo/menus/HomeMenu.java
@@ -8,12 +8,14 @@ public class HomeMenu extends MenuParent {
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/homemenu.png";
     private static final String PLAY_BUTTON1 = "/com/example/demo/images/Buttons/play2.png";
     private static final String PLAY_BUTTON2 = "/com/example/demo/images/Buttons/play1.png";
+    private static final String EXIT_BUTTON1 = "/com/example/demo/images/Buttons/exit1.png";
+    private static final String EXIT_BUTTON2 = "/com/example/demo/images/Buttons/exit2.png";
 
     public HomeMenu(Stage stage, double screenHeight, double screenWidth) {
         super(stage, BACKGROUND_IMAGE_NAME, screenHeight, screenWidth);
 
         double posX = (stage.getWidth() - 600) / 2;
-        double posY = (stage.getHeight() - 100) / 2;
+        double playPosY = (stage.getHeight() - 175) / 2;
 
         double playButtonWidth = 549;
         double playButtonHeight = 48;
@@ -23,8 +25,24 @@ public class HomeMenu extends MenuParent {
             public void handle(ActionEvent event) {
                 goToNextLevel("LevelOne");
             }
-        }, posX, posY, playButtonWidth, playButtonHeight);
+        }, posX, playPosY, playButtonWidth, playButtonHeight);
+
+        double exitButtonWidth = 549;
+        double exitButtonHeight = 48;
+
+        double exitPosY = (stage.getHeight() - 0) / 2;
+
+        buttonImage(EXIT_BUTTON1, EXIT_BUTTON2, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                exitGame();
+            }
+        }, posX, exitPosY, exitButtonWidth, exitButtonHeight);
+
     }
 
+    private void exitGame() {
+        System.exit(0);
+    }
 
 }
